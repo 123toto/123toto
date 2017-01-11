56b52498aaa426af72753c0783ac6a7a3fe6ab5f
123toto <berthost91@gmail.com>
2017-01-11 17:43:42 +0100

    premier ajout _JAVASCRIPT sur GitHub

---
+++ _JAVASCRIPT/Jasmin/Javascript-Recall/questions.js
@@ -0,0 +1,101 @@
+var selectElementsStartingWithA = function(array) {
+}
+
+var selectElementsStartingWithVowel = function(array) {
+}
+
+var removeNullElements = function(array) {
+}
+
+var removeNullAndFalseElements = function(array) {
+}
+
+var reverseWordsInArray = function(array) {
+}
+
+var everyPossiblePair = function(array) {
+}
+
+var allElementsExceptFirstThree = function(array) {
+}
+
+var addElementToBeginning = function(array, element) {
+}
+
+var sortByLastLetter = function(array) {
+}
+
+var getFirstHalf = function(string) {
+}
+
+var makeNegative = function(number) {
+}
+
+var numberOfPalindromes = function(array) {
+}
+
+var shortestWord = function(array) {
+}
+
+var longestWord = function(array) {
+}
+
+var sumNumbers = function(array) {
+}
+
+var repeatElements = function(array) {
+}
+
+var stringToNumber = function(string) {
+}
+
+var calculateAverage = function(array) {
+}
+
+var getElementsUntilGreaterThanFive = function(array) {
+}
+
+var convertArrayToObject = function(array) {
+}
+
+var getAllLetters = function(array) {
+}
+
+var swapKeysAndValues = function(object) {
+}
+
+var sumKeysAndValues = function(object) {
+}
+
+var removeCapitals = function(string) {
+}
+
+var roundUp = function(number) {
+}
+
+var formatDateNicely = function(date) {
+}
+
+var getDomainName = function(string) {
+}
+
+var titleize = function(string) {
+}
+
+var checkForSpecialCharacters = function(string) {
+}
+
+var squareRoot = function(number) {
+}
+
+var factorial = function(number) {
+}
+
+var findAnagrams = function(string) {
+}
+
+var convertToCelsius = function(number) {
+}
+
+var letterPosition = function(array) {
+}
